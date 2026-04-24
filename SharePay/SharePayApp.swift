a8dabba7be0fc74bb2bb032bf70221fce89c2366
Nick Nedjat <nick.nedjat@icloud.com>
2026-04-24 19:59:05 -0400

    Initial Commit

---
+++ SharePay/SharePayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharePayApp.swift
+//  SharePay
+//
+//  Created by Nick Nedjat on 24.04.26.
+//
+
+import SwiftUI
+
+@main
+struct SharePayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
